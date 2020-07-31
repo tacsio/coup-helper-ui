ec3cd21f92ce1604f347d272617c452372e08511
Tarcisio Coutinho <tcs5cin@gmail.com>
2020-07-31 02:35:21 -0300

    deploy

---
+++ precache-manifest.0bb2abb75fed043f1fe2a354a8bd4505.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3550c1ff03116b61352",
+    "url": "./static/js/app.7fc33f27.chunk.js"
+  },
+  {
+    "revision": "3fcf8173ae24680194db",
+    "url": "./static/js/runtime~app.29ed512f.js"
+  },
+  {
+    "revision": "139f37ca4adc04c98148",
+    "url": "./static/js/2.1ed6f10b.chunk.js"
+  },
+  {
+    "revision": "a08f447866a1603616a032c3444ee5bf",
+    "url": "./static/media/avatar1.a08f4478.png"
+  },
+  {
+    "revision": "2677abc1c45b4fe54e91353e98b1debc",
+    "url": "./static/media/avatar2.2677abc1.png"
+  },
+  {
+    "revision": "1cf41ecdfa7e28e3bfd76686f9e09db8",
+    "url": "./static/media/avatar3.1cf41ecd.png"
+  },
+  {
+    "revision": "232fd21677a1d6099a93e077d5c01dcc",
+    "url": "./static/media/avatar4.232fd216.png"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "././fonts/Feather.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "././fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "././fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "././fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "././fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "././fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "././fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "././fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "././fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "././fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "././fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "././fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "./serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "./expo-service-worker.js"
+  },
+  {
+    "revision": "708546c42258e228e516900c49412279",
+    "url": "./favicon-16.png"
+  },
+  {
+    "revision": "12c99660fc1b59c72b89df2948a04089",
+    "url": "./favicon-32.png"
+  },
+  {
+    "revision": "aff75a569220b7304814ad35f8a70437",
+    "url": "./favicon.ico"
+  },
+  {
+    "revision": "d087413a5742db47e70191c71b59c55b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "36f41971b27442e9033799406c8783d9",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "d540f5fcbad5c5b1e34efee1e7025eb4",
+    "url": "./register-service-worker.js"
+  }
+];
